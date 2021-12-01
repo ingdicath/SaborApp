d88322e8d48c2d491bdc2268a9832a03caf7447f
JorgeVargas <javargas216@unicauca.edu.co>
2021-12-01 12:13:12 -0500

    P1G4MIN-41 Re diseño de la interfaz del menú

--- app/src/main/java/com/example/saborapp/MenuActivity.kt
+++ app/src/main/java/com/example/saborapp/MenuActivity.kt
@@ -4,15 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        val btnRecipe = findViewById<Button>(R.id.btnRecipe)
-        val btnProfile = findViewById<Button>(R.id.btnProfile)
-        val btnAboutUs = findViewById<Button>(R.id.btnAboutUs)
+        val btnRecipe = findViewById<ImageButton>(R.id.btnRecipe)
+        val btnProfile = findViewById<ImageButton>(R.id.btnProfile)
+        val btnAboutUs = findViewById<ImageButton>(R.id.btnAboutUs)
 
         btnRecipe.setOnClickListener {
             startActivity(Intent(this,MainActivity::class.java))
