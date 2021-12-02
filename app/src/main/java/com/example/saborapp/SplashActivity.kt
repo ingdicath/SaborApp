f7323b3ff56e76e7118bf2218fed7d6732004cc7
Diana Salamanca <dianitasale@gmail.com>
2021-12-02 16:31:42 +0100

    Cambiar diseno boton registrate, remover texto en image button mi perfil

--- app/src/main/java/com/example/saborapp/SplashActivity.kt
+++ app/src/main/java/com/example/saborapp/SplashActivity.kt
@@ -3,11 +3,17 @@ package com.example.saborapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        startActivity(Intent(this, LoginActivity::class.java))
-        finish()
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		// Delay for splash screen
+//		val timer: Long = 1400
+//		Handler().postDelayed({ startActivity(Intent(this, LoginActivity::class.java)) }, timer)
+
+		startActivity(Intent(this, LoginActivity::class.java))
+		finish()
+	}
 }
